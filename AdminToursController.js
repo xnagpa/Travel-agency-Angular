ff0c42d8c9a17050eb2e787aba9f69ab67a9dec3
mglinka <m.glinka@rambler-co.ru>
2015-10-26 18:03:22 +0300

    Adds requested functionality

---
+++ AdminToursController.js
@@ -0,0 +1,30 @@
+angular.module('thSample').controller('AdminToursController', function($scope, currentUser){
+  console.log(currentUser);
+  $scope.form_hidden = true;
+  $scope.tours = allTours;
+  $scope.newTour =  {
+      title: null,
+      edit_mode:true,
+      country: null,
+      text: null,
+      price: null
+
+    };
+
+  $scope.addTour = function(){
+    $scope.tours.push(angular.copy($scope.newTour));
+  };
+
+  $scope.toggleForm = function(){
+    $scope.form_hidden = !$scope.form_hidden
+  };
+
+  $scope.changeEditMode = function(tour){
+    tour.edit_mode = !tour.edit_mode
+  };
+
+   $scope.removeItem = function(index){
+    $scope.tours.splice(index, 1);
+  }
+
+});
--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 var app = angular.module('thSample',['ngRoute']).
 config(function($routeProvider, $locationProvider){
-  $routeProvider.when('/',{
-    templateUrl:"list.html",
-    controller:"MountsController",
+  $routeProvider.when('/edit-tours',{
+    templateUrl:"edit-tours.html",
+    controller:"AdminToursController",
     publicAccess: true,
     resolve: {
       currentUser: function(){
-        return {name: 'Alex'};
+        return {name: 'Admin'};
       }
     }
   })
   .when('/tour/:slug',{
     templateUrl:"tour.html",
     controller:"TourController",
-    publicAccess: false
+    publicAccess: true
+  })
+  .when('/edit-countries',{
+    templateUrl:"edit-countries.html",
+    controller:"AdminCountryController",
+    publicAccess: true
+  })
+  .when('/',{
+    templateUrl:"tours.html",
+    controller:"ToursController",
+    publicAccess: true
   })
   .otherwise({
     redirectTo: '/'
@@ -38,7 +48,7 @@ config(function($routeProvider, $locationProvider){
 allTours = [
     {
       title: "Yet another suburban train",
-      edit_mode:false,
+      edit_mode:true,
       country: "Russia",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       slug: 1,
@@ -46,10 +56,22 @@ allTours = [
     },
     {
       title: "Adventure time",
-      edit_mode:false,
+      edit_mode:true,
       country: "USA",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       slug: 2,
       price: 100000
     }
 ];
+
+allCountries = [
+  {
+    title: "Russia",
+    edit_mode:true
+  },
+  {
+    title: "Poland",
+    edit_mode:true
+  }
+];
+
